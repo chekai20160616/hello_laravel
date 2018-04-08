4a723d132643d02a4a66d4d3aca02dca17ae4530
chekai <786401300@qq.com>
2018-04-08 15:43:58 +0800

    test

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', '786401300@qq.com'),
+        'name' => env('MAIL_FROM_NAME', 'chekai'),
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,18 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+
+Route::get('mail', function () {
+    $name = '学院君';
+        $flag = Mail::send('emails.confirm',['name'=>$name],function($message){
+            $to = 'kevin.che@onemena.com';
+            $message ->to($to)->subject('测试邮件');
+        });
+        if($flag){
+            echo '发送邮件成功，请查收！';
+        }else{
+            echo '发送邮件失败，请重试！';
+        }
+
+});
